38418eab14309bb96511f38857b281620dda8bf8
Abijah Stokes <abijahstokes@gmail.com>
2018-10-08 14:02:20 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space != " "
+      counter += 1
+    end
+  end
+  counter
+end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board [index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    puts "X"
+  else
+    puts "O"
+end
